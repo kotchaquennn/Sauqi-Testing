7c13217e0df4d486222d04044c35cfde22fce517
kotchaquennn <irvanbundaran@gmail.com>
2024-10-03 17:34:25 +0700

    komentar.dart

---
+++ bin/Komentar.dart
@@ -0,0 +1,12 @@
+
+//ini komentar satu baris
+///Documentation, ini adalah jenis komentar yang 
+///biasanya digunakan sebagai  dart documentation
+void main(){
+  String name;
+  name = 'sauqi';
+  print("Hello,$name!");
+}
+
+/*ini adalah komentar
+lebih dari satu baris atau multi-line*/
